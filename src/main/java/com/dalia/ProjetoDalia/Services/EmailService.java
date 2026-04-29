e6d1e34141dae57909e9e8a01fc4549a1b8f4c68
Ana Beatriz <ana.alves69@fatec.sp.gov.br>
2026-04-29 17:44:20 -0300

    mudando emailserver pra resend

--- src/main/java/com/dalia/ProjetoDalia/Services/EmailService.java
+++ src/main/java/com/dalia/ProjetoDalia/Services/EmailService.java
@@ -1,7 +1,11 @@
 package com.dalia.ProjetoDalia.Services;
 
 
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -11,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
+    @Value("${API_KEY}")
+    private String apiKey;
 
     //logica da denuncia que envia o email pra delegacia da mulher (vulgo guilherme)
     public void enviarDenuncia(String conteudo) {
@@ -24,13 +30,22 @@ public class EmailService {
     @Async
     //envia o email com o codigo para a usuaria
     public void sendToken(String to, String token){
-        SimpleMailMessage message =  new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Codigo de verificação - Dalia");
-        message.setText("Olá! \nSeu codigo de verificação para o app Dalia Calendario menstrual é: \n"
-                        + token +
-                        "\nEste codigo expira em 15 minutos.");
-        mailSender.send(message);
+        Resend resend = new Resend(apiKey);
+
+        CreateEmailOptions params = CreateEmailOptions.builder()
+            .from("Dalia <onboarding@resend.dev>")
+            .to(to)
+            .subject("Codigo de verificação - Dalia")
+            .html("Olá! \nSeu codigo de verificação para o app Dalia Calendario menstrual é: \n"
+            + token +
+            "\nEste codigo expira em 15 minutos.")
+            .build();
+        try{
+            resend.emails().send(params);
+            System.out.println("Email enviado com sucesso!");
+        } catch (ResendException e) {
+            System.out.println("Email enviado com erro!" + e.getMessage());
+        }
     }
 }
 
