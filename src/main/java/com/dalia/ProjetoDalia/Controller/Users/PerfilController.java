4d8c4690ef4c2eb5bb010f7eb69a253ea2e70108
Ana Beatriz <164010836+ana-bia07@users.noreply.github.com>
2026-05-14 17:15:47 -0300

    Update PerfilController.java

--- src/main/java/com/dalia/ProjetoDalia/Controller/Users/PerfilController.java
+++ src/main/java/com/dalia/ProjetoDalia/Controller/Users/PerfilController.java
@@ -66,11 +66,6 @@ public class PerfilController {
                 user.getSearch().setContraceptiveType(userDTO.search().getContraceptiveType());
             }
 
-            user.getSearch().setAge(userDTO.search().getAge());
-            user.getPregnancyMonitoring().setDayPregnancy(userDTO.pregnancyMonitoring().getDayPregnancy());
-            user.getPregnancyMonitoring().setGestationWeeks(userDTO.pregnancyMonitoring().getGestationWeeks());
-            user.getPregnancyMonitoring().setExpectedBirthDate(userDTO.pregnancyMonitoring().getExpectedBirthDate());
-
             if (userDTO.pregnancyMonitoring() != null) {
                 user.getPregnancyMonitoring().setDayPregnancy(userDTO.pregnancyMonitoring().getDayPregnancy());
                 user.getPregnancyMonitoring().setGestationWeeks(userDTO.pregnancyMonitoring().getGestationWeeks());
