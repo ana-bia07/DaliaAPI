29def5bd5fd90154ae8c1093a2f051f2df88737c
Ana Beatriz <ana.alves69@fatec.sp.gov.br>
2026-09-12 23:55:27 -0300

    fix repoEvent: list

--- src/main/java/com/dalia/ProjetoDalia/Model/Repository/EventRespository.java
+++ src/main/java/com/dalia/ProjetoDalia/Model/Repository/EventRespository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 
 public interface EventRespository extends MongoRepository<Event, String> {
-    Optional<Event> findByIdUser(String id);
+    Optional<List<Event>> findByIdUser(String id);
 
 }
